da59fc71099f7249caf793c28dcb47ec52eb677c
Ananda Prayoga Kusuma <prayogaanant61@gmail.com>
2024-12-02 09:29:30 +0700

    first

---
+++ application/modules/student/views/hasil_filter.php
@@ -0,0 +1,20 @@
+<!-- application/views/siswa/hasil_filter.php -->
+<!-- <h2>Hasil Filter Siswa</h2>
+<table>
+    <thead>
+        <tr>
+            <th>No.</th>
+            <th>Nama Siswa</th>
+            <!-- Tambahkan kolom lain sesuai kebutuhan -->
+        </tr>
+    </thead>
+    <tbody>
+        <?php $no = 1; foreach ($student as $student): ?>
+            <tr>
+                <td><?= $no++ ?></td>
+                <td><?= $student->student ?></td>
+                <!-- Tambahkan kolom lain sesuai kebutuhan -->
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table> -->
